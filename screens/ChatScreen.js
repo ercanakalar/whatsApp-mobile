62f35912db1c1f04bed6673d267e4feafd3b9d1f
ercanakalar1995 <05210000922@ogrenci.ege.edu.tr>
2023-04-26 10:22:58 +0300

    see the message box

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -9,43 +9,43 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Feather } from "@expo/vector-icons";
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather } from '@expo/vector-icons';
 
-import backgroundImage from "../assets/images/droplet.jpeg";
-import colors from "../constants/colors";
-import { useSelector } from "react-redux";
-import PageContainer from "../components/PageContainer";
-import Bubble from "../components/Bubble";
-import { createChat } from "../utils/actions/chatActions";
+import backgroundImage from '../assets/images/droplet.jpeg';
+import colors from '../constants/colors';
+import { useSelector } from 'react-redux';
+import PageContainer from '../components/PageContainer';
+import Bubble from '../components/Bubble';
+import { createChat } from '../utils/actions/chatActions';
 
 const ChatScreen = (props) => {
-  const userData = useSelector(state => state.auth.userData);
-  const storedUsers = useSelector(state => state.users.storedUsers);
-  
+  const userData = useSelector((state) => state.auth.userData);
+  const storedUsers = useSelector((state) => state.users.storedUsers);
+  const storedChats = useSelector((state) => state.chats.chatsData);
+
   const [chatUsers, setChatUsers] = useState([]);
-  const [messageText, setMessageText] = useState("");
+  const [messageText, setMessageText] = useState('');
   const [chatId, setChatId] = useState(props.route?.params?.chatId);
 
-  const chatData = props.route?.params?.newChatData;
+  const chatData = (chatId && storedChats[chatId]) || props.route?.params?.newChatData;
 
   const getChatTitleFromName = () => {
-    const otherUserId = chatUsers.find(uid => uid !== userData.userId);
+    const otherUserId = chatUsers.find((uid) => uid !== userData.userId);
     const otherUserData = storedUsers[otherUserId];
 
     return otherUserData && `${otherUserData.firstName} ${otherUserData.lastName}`;
-  }
+  };
 
   useEffect(() => {
     props.navigation.setOptions({
-      headerTitle: getChatTitleFromName()
-    })
-    setChatUsers(chatData.users)
-  }, [chatUsers])
+      headerTitle: getChatTitleFromName(),
+    });
+    setChatUsers(chatData.users);
+  }, [chatUsers]);
 
   const sendMessage = useCallback(async () => {
-
     try {
       let id = chatId;
       if (!id) {
@@ -53,41 +53,26 @@ const ChatScreen = (props) => {
         id = await createChat(userData.userId, props.route.params.newChatData);
         setChatId(id);
       }
+    } catch (error) {}
 
-
-    } catch (error) {
-      
-    }
-
-
-    setMessageText("");
+    setMessageText('');
   }, [messageText, chatId]);
 
   return (
-    <SafeAreaView edges={["right", "left", "bottom"]} style={styles.container}>
+    <SafeAreaView edges={['right', 'left', 'bottom']} style={styles.container}>
       <KeyboardAvoidingView
         style={styles.screen}
-        behavior={ Platform.OS === "ios" ? "padding" : undefined}
-        keyboardVerticalOffset={100}>
-        <ImageBackground
-          source={backgroundImage}
-          style={styles.backgroundImage}
-        >
-          <PageContainer style={{ backgroundColor: 'transparent'}}>
-
-            {
-              !chatId && <Bubble text='This is a new chat. Say hi!' type="system" />
-            }
-
-
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={100}
+      >
+        <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+          <PageContainer style={{ backgroundColor: 'transparent' }}>
+            {!chatId && <Bubble text="This is a new chat. Say hi!" type="system" />}
           </PageContainer>
         </ImageBackground>
 
         <View style={styles.inputContainer}>
-          <TouchableOpacity
-            style={styles.mediaButton}
-            onPress={() => console.log("Pressed!")}
-          >
+          <TouchableOpacity style={styles.mediaButton} onPress={() => console.log('Pressed!')}>
             <Feather name="plus" size={24} color={colors.blue} />
           </TouchableOpacity>
 
@@ -98,21 +83,18 @@ const ChatScreen = (props) => {
             onSubmitEditing={sendMessage}
           />
 
-          {messageText === "" && (
-            <TouchableOpacity
-              style={styles.mediaButton}
-              onPress={() => console.log("Pressed!")}
-            >
+          {messageText === '' && (
+            <TouchableOpacity style={styles.mediaButton} onPress={() => console.log('Pressed!')}>
               <Feather name="camera" size={24} color={colors.blue} />
             </TouchableOpacity>
           )}
 
-          {messageText !== "" && (
+          {messageText !== '' && (
             <TouchableOpacity
               style={{ ...styles.mediaButton, ...styles.sendButton }}
               onPress={sendMessage}
             >
-              <Feather name="send" size={20} color={"white"} />
+              <Feather name="send" size={20} color={'white'} />
             </TouchableOpacity>
           )}
         </View>
@@ -124,16 +106,16 @@ const ChatScreen = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   screen: {
-    flex: 1
+    flex: 1,
   },
   backgroundImage: {
     flex: 1,
   },
   inputContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     paddingVertical: 8,
     paddingHorizontal: 10,
     height: 50,
@@ -147,8 +129,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
   },
   mediaButton: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     width: 35,
   },
   sendButton: {
